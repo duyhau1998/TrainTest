ab31fe0f331e40b5a50b8e74f9b1fa48cb70f438
Duy Hau <vunguyenduyhau1998@gmail.com>
2018-07-19 22:26:58 +0700

    first commit

---
+++ TrainTest/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using TrainTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+
+namespace TrainTest.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+    }
+}
